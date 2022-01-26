08e957404bf66b16496ef8269b0e4d86c0fe013e
matheo44 <64661992+matheo44@users.noreply.github.com>
2022-01-26 21:57:57 +0100

    add: initialCommit

---
+++ Portfolio/src/components/App.js
@@ -0,0 +1,17 @@
+
+import logo from '../assets/favicon.ico';
+
+function App() {
+
+ 
+
+  return (
+    <div>  
+      
+      <img src={logo} alt="La maison jungle" className="lmj-logo" />
+      
+    </div>
+  );
+}
+
+export default App;
